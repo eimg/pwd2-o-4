d477098def099dc4f0e0e8f3c6d0ff14e85229cc
Ei Maung <eimg@fairwayweb.com>
2026-06-13 08:59:02 +0630

    react hook form

--- social-app/src/pages/Login.jsx
+++ social-app/src/pages/Login.jsx
@@ -1,7 +1,45 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, OutlinedInput, Typography } from "@mui/material";
+
+import { useForm } from "react-hook-form";
 
 export default function Login() {
-    return <Box>
-        <Typography>Login</Typography>
-    </Box>
+    const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+
+	const login = data => console.log(data);
+
+	return (
+		<Box>
+			<Typography variant="h3">Login</Typography>
+			<form onSubmit={handleSubmit(login)}>
+				<OutlinedInput
+					fullWidth
+					sx={{ mt: 2 }}
+					placeholder="username"
+					{...register("username", { required: true })}
+                    error={errors.username}
+				/>
+
+				<OutlinedInput
+					fullWidth
+					sx={{ mt: 2 }}
+					type="password"
+					placeholder="password"
+					{...register("password", { required: true })}
+                    error={errors.password}
+				/>
+
+				<Button
+					sx={{ mt: 2 }}
+					fullWidth
+					type="submit"
+					variant="contained">
+					Login
+				</Button>
+			</form>
+		</Box>
+	);
 }
--- social-app/src/pages/Register.jsx
+++ social-app/src/pages/Register.jsx
@@ -1,9 +1,60 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, OutlinedInput, Typography } from "@mui/material";
+
+import { useForm } from "react-hook-form";
 
 export default function Register() {
-	return (
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const create = data => console.log(data);
+
+    return (
 		<Box>
-			<Typography>Register</Typography>
+			<Typography variant="h3">Register</Typography>
+			<form onSubmit={handleSubmit(create)}>
+				<OutlinedInput
+					fullWidth
+					sx={{ mt: 2 }}
+					placeholder="name"
+					{...register("name", { required: true })}
+					error={errors.name}
+				/>
+
+				<OutlinedInput
+					fullWidth
+					sx={{ mt: 2 }}
+					placeholder="username"
+					{...register("username", { required: true })}
+					error={errors.username}
+				/>
+
+				<OutlinedInput
+					fullWidth
+					sx={{ mt: 2 }}
+					placeholder="bio"
+					{...register("bio")}
+				/>
+
+				<OutlinedInput
+					fullWidth
+					sx={{ mt: 2 }}
+					type="password"
+					placeholder="password"
+					{...register("password", { required: true })}
+					error={errors.password}
+				/>
+
+				<Button
+					sx={{ mt: 2 }}
+					fullWidth
+					type="submit"
+					variant="contained">
+					Register
+				</Button>
+			</form>
 		</Box>
 	);
 }
